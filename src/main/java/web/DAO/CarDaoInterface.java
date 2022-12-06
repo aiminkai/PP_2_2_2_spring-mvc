7f353f5cbb178525edbeac5125c771b7ce0b9aab
aiminkai <m.a.azarka@gmail.com>
2022-12-06 10:13:58 +0300

    added CarDaoInterface ..

--- src/main/java/web/DAO/CarDaoInterface.java
+++ src/main/java/web/DAO/CarDaoInterface.java
@@ -1,4 +1,11 @@
 package web.DAO;
 
+import web.models.Car;
+
+import java.util.List;
+
 public interface CarDaoInterface {
+    public List<Car> getCars();
+    public void setCars(List<Car> cars);
+
 }
